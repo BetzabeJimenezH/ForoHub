e901aa874ec5b661bda58636b12e9c90ffac121b
Betzabé Jiménez Hernández <162236694+BetzabeJimenezH@users.noreply.github.com>
2025-01-21 18:00:57 -0600

    Add files via upload

---
+++ forohub/src/main/java/com/desafio/forohub/domain/respuesta/validations/create/ValidarRespuestaCreada.java
@@ -0,0 +1,4 @@
+package com.desafio.forohub.domain.respuesta.validations.create;
+
+public interface ValidarRespuestaCreada {
+}
---
+++ forohub/src/main/java/com/desafio/forohub/domain/respuesta/validations/update/ValidarRespuestaActualizada.java
@@ -0,0 +1,4 @@
+package com.desafio.forohub.domain.respuesta.validations.update;
+
+public interface ValidarRespuestaActualizada {
+}
